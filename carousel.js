4a3f906c3c74236e35db6c718a5d867974791be3
JeremyC <jeremy4322@gmail.com>
2022-10-10 19:24:31 -0400

    partly coded the carousel

--- carousel.js
+++ carousel.js
@@ -1,1 +1,22 @@
-console.log('test')
+//give carousel buttons backwards and forwards functionality
+
+
+const buttons = document.querySelectorAll("[data-button-direction]") //select all buttons
+
+//apply on click listener to each button
+buttons.forEach(button => {
+  button.addEventListener("click", ()=> {
+    if (button.dataset.buttonDirection === "next") { 
+      //if buttons have data 'next', variable direction is 1. if buttons are 'back', variable is -1. 
+      const direction = 1
+      const selectedCarousel = button.closest("[data-carousel]").children[0]//store in a variable the parent carousel of the clicked button
+      const activeSlide = selectedCarousel.querySelector("[data-active]")//store in a variable the active slide in the carousel
+      console.log(activeSlide)
+    } else if (button.dataset.buttonDirection === "back") {
+      const direction = -1
+      const carousel = button.closest("[data-carousel]").querySelector
+
+      console.log(button.closest("[data-carousel]").children[0])
+    }
+  })
+})
